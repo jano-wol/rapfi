e2341f9aaa76336d6b0938b9415ce9e6bf638996
Andrzej Polak <jjjkamu@gmail.com>
2024-10-25 13:38:26 +0200

    Add build type

--- Rapfi/scripts/configure.sh
+++ Rapfi/scripts/configure.sh
@@ -4,5 +4,5 @@ source "$(dirname "${0}")/init.sh"
 rm -rf "${BUILD_FOLDER}"
 mkdir -p "${BUILD_FOLDER}"
 cd "${BUILD_FOLDER}"
-cmake "${RAPFI_FOLDER}" -DCMAKE_BUILD_TYPE=Release
+cmake "${RAPFI_FOLDER}" "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"
 
--- Rapfi/scripts/init.sh
+++ Rapfi/scripts/init.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -ex	
+if [[ $1 != "debug" ]] && [[ $1 != "release" ]] && [[ $1 != "relwithdebinfo" ]]; then
+    echo "First command line argument should be debug or release or relwithdebinfo"
+    exit 1
+fi	
+if [[ $2 != "" ]] && [[ $2 != "oldarch" ]]; then
+    echo "Second command line argument should be <empty string> or oldarch"
+    exit 1
+fi	
+CMAKE_BUILD_TYPE=$1
 SCRIPT_FOLDER=$(dirname "${0}") 
 RAPFI_FOLDER=$(readlink -e "${SCRIPT_FOLDER}/../")
-BUILD_FOLDER="${RAPFI_FOLDER}/build/"
+BUILD_FOLDER="${RAPFI_FOLDER}/build/${CMAKE_BUILD_TYPE}"
 
