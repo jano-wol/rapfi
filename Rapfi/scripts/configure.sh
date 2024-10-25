fbc09c7de1a21f4e2e4beaf713787dddff91be2d
Andrzej Polak <jjjkamu@gmail.com>
2024-10-25 14:00:15 +0200

    Set default compiler to clang

--- Rapfi/scripts/configure.sh
+++ Rapfi/scripts/configure.sh
@@ -4,5 +4,5 @@ source "$(dirname "${0}")/init.sh"
 rm -rf "${BUILD_FOLDER}"
 mkdir -p "${BUILD_FOLDER}"
 cd "${BUILD_FOLDER}"
-cmake "${RAPFI_FOLDER}" "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"
+cmake "${RAPFI_FOLDER}" "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}" "-DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}"
 
--- Rapfi/scripts/init.sh
+++ Rapfi/scripts/init.sh
@@ -9,6 +9,8 @@ if [[ $2 != "" ]] && [[ $2 != "oldarch" ]]; then
     exit 1
 fi	
 CMAKE_BUILD_TYPE=$1
+CMAKE_CXX_COMPILER=clang++ # g++ is also possible
+
 SCRIPT_FOLDER=$(dirname "${0}") 
 RAPFI_FOLDER=$(readlink -e "${SCRIPT_FOLDER}/../")
 BUILD_FOLDER="${RAPFI_FOLDER}/build/${CMAKE_BUILD_TYPE}"
