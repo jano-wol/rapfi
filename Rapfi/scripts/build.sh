beb1b3495b638e966dff28eb37dc415b1aa57b23
janowol <jjjkamu@gmail.com>
2024-10-27 10:34:39 +0100

    Copy data needed in runtime

--- Rapfi/scripts/build.sh
+++ Rapfi/scripts/build.sh
@@ -3,3 +3,14 @@ set -ex
 source "$(dirname "${0}")/init.sh"
 cmake --build "${BUILD_FOLDER}" -j 4 -v
 
+NETWORK_FOLDER="${RAPFI_FOLDER}/../Networks/"
+CONFIG_FOLDER="${NETWORK_FOLDER}/config-example/"
+CLASSICAL_FOLDER="${NETWORK_FOLDER}/classical/"
+MIX9_FOLDER="${NETWORK_FOLDER}/mix9nnue/"
+
+if [ $? -eq 0 ]; then
+    cp ${CLASSICAL_FOLDER}/* "${BUILD_FOLDER}"
+    cp ${MIX9_FOLDER}/* "${BUILD_FOLDER}"
+    cp ${CONFIG_FOLDER}/config.toml "${BUILD_FOLDER}"
+fi
+
